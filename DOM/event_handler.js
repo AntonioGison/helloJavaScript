fc3646028b3a356cf97063ac098229400902f1f3
Antonio Gison <antonio@gison.it>
2023-05-31 17:27:07 +0100

    piano keys completed

--- DOM/event_handler.js
+++ DOM/event_handler.js
@@ -3,7 +3,7 @@
 let readMore = document.getElementById('read-more');
 let moreInfo = document.getElementById('more-info');
 
-// Write your code here:
+
 function showInfo(){
   moreInfo.style.display = 'block';
 }
