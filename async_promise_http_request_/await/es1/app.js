91b9f4d462a3cb5460af5e174565beb8fa2cd7b4
Antonio Gison <antonio@gison.it>
2023-06-09 17:58:27 +0100

    async

--- async_promise_http_request_/await/es1/app.js
+++ async_promise_http_request_/await/es1/app.js
@@ -8,11 +8,12 @@ function nativePromiseDinner() {
   });
 }
 
-//await is an operator: it returns the resolved value of a promise
+//await is an operator: it returns the resolved value of a promise, it substite the native version with then
+// you don't need then
 // async/await version:
 async function announceDinner() {
   // Write your code below:
-  let resolvedValue = await brainstormDinner();
+  let resolvedValue = await brainstormDinner();//don't forget to write async because await on it's own doesn't work
   console.log(`I'm going to make ${resolvedValue} for dinner.`);
 }
 
