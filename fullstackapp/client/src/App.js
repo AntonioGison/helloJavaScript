9063bfd4558571d5e5f3df069cf64bf55a9b1975
Antonio Gison <antonio@gison.it>
2023-07-28 15:11:46 +0200

    fullstack app todo

---
+++ fullstackapp/client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import InputTodo from './components/InputTodo';
+function App() {
+  return (
+    <div className="App">
+      <InputTodo></InputTodo>
+    </div>
+  );
+}
+
+export default App;
